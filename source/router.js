e83b081b1ad2036cc2b2693a02e45b02dcc32d2d
papebsow <papebilalysow@gmail.com>
2024-07-22 09:09:44 +0000

    🛠️ initilize the routing feature

--- source/router.js
+++ source/router.js
@@ -2,4 +2,30 @@ export default class Router {
     constructor(routes) {
         this.routes = routes;
     }
+
+    _matchUrlToRoute(urlSegments) {
+        const matchedRoute = this.routes.find(route => {
+            const routePathSegments = route.path.split('/').slice(1);
+            if (routePathSegments.length !== urlSegments.length) {
+                return false;
+            }
+            return routePathSegments.every((routePathSegment, i) => routePathSegment === urlSegments[i]);
+        });
+        return matchedRoute;
+    }
+
+    _loadInitialRoute() {
+        const pathSegments = window.location.pathname.split('/').slice(1);
+        this.loadRoute(...pathSegments);
+    }
+
+    loadRoute(...urlSegments) {
+        const matchedRoute = this._matchUrlToRoute(urlSegments);
+
+        const url = `/${urlSegments.join('/')}`;
+        history.pushState({}, '', url);
+
+        const routerOutElement = document.querySelector('[data-router]');
+        routerOutElement.innerHTML = matchedRoute.template;
+    }
 }
