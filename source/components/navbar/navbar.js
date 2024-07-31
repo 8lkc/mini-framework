e55b1165391b033b92094759a8c922be81238daf
8lkc <papebilalysow@gmail.com>
2024-07-31 11:39:33 +0000

    🛠️ some updates

--- source/components/navbar/navbar.js
+++ source/components/navbar/navbar.js
@@ -2,7 +2,6 @@ import Component from "../global.js"
 
 export default class NavbarComponent extends Component {
     constructor(name, shadowRoot) {
-        const model = {};
-        super(name, shadowRoot, model);
+        super(name, shadowRoot);
     }
 }
